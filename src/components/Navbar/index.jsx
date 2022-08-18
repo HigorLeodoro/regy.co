946c01244e9babe23273120c62e77f5c024ef36e
Higor Leodoro <higorscreamo@gmail.com>
2022-08-18 18:04:11 -0300

    priemiro commit

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,25 +1,28 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from './styles.module.css';
 
 import logo from '../../assets/logo.png'
 
+
+
 export default function Navbar() {
  return (
    <div>
-      <header>
+      <header className={styles.fixed}>
         <nav className={styles.nav}>
-          <a href='/'> <Image src={logo} width={200} height={100} /></a>
+          <div className={styles.logo}><a href='/'> <Image src={logo} width={200} height={125} /></a></div>
           <div className={styles.mobileMenu}>
             <div className={styles.line1}></div>
             <div className={styles.line2}></div>
             <div className={styles.line3}></div>
           </div>
           <ul className={styles.navList}>
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/about">Sobre</a></li>
-            <li><a href="/">Projetos</a></li>
-            <li><a href="/">Contato</a></li>
+            <li><Link href="/">Inicio</Link></li>
+            <li><Link href="/">Quem somos</Link></li>
+            {/* <li><Link href="/">Projetos</Link></li> */}
+            <li><Link href="/">Contato</Link></li>
           </ul>
         </nav>
       </header>
